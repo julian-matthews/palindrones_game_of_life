23e45e5d2fcb9df9e01f5425235ff18ab76a71e0
Julian Matthews <julian.r.matthews@gmail.com>
2024-06-03 17:30:26 +0900

    updates to game of life

--- code/generate_board.m
+++ code/generate_board.m
@@ -1,10 +1,14 @@
 %% PALINDRONE'S GAME OF LIFE: generate board
 % Resize the board and add border
 
-function the_new_board = generate_board( the_board, cell_size )
+function the_new_board = generate_board( the_board, cell_size, the_border )
 
-% Border size
-border_size = 1;
+% Define border size
+if nargin < 3
+    border_size = 0;
+else
+    border_size = the_border;
+end
 
 % Resize by factor of 'cell_size'
 replicate_matrix = ones(cell_size, cell_size);
@@ -12,22 +16,26 @@ replicate_matrix = ones(cell_size, cell_size);
 % Use the kron function to resize the matrix
 the_new_board = kron(the_board, replicate_matrix);
 
-% Overwrite the last two rows and columns of each 10x10 block with zeros
-for i = 1:(size(the_new_board,1)/cell_size)
-    for j = 1:(size(the_new_board,2)/cell_size)
-        % Determine the block's start and end indices in the resized matrix
-        start_row = (i-1)*cell_size + 1;
-        end_row = start_row + 9;
-        start_col = (j-1)*cell_size + 1;
-        end_col = start_col + 9;
-        
-        % Set the last X rows and last two columns of each block to 0
-        the_new_board(end_row - (border_size-1) ...
-            :end_row, start_col:end_col) = 0;
-        the_new_board(start_row:end_row, ...
-            end_col - (border_size-1) ...
-            :end_col) = 0;
+if border_size > 0
+    
+    % Overwrite the last two rows and columns of each 10x10 block with zeros
+    for i = 1:(size(the_new_board,1)/cell_size)
+        for j = 1:(size(the_new_board,2)/cell_size)
+            % Determine the block's start and end indices in the resized matrix
+            start_row = (i-1)*cell_size + 1;
+            end_row = start_row + 9;
+            start_col = (j-1)*cell_size + 1;
+            end_col = start_col + 9;
+            
+            % Set the last X rows and last two columns of each block to 0
+            the_new_board(end_row - (border_size-1) ...
+                :end_row, start_col:end_col) = 0;
+            the_new_board(start_row:end_row, ...
+                end_col - (border_size-1) ...
+                :end_col) = 0;
+        end
     end
+    
 end
 
 end
--- code/palindrone_game_of_life.m
+++ code/palindrone_game_of_life.m
@@ -11,8 +11,11 @@ take_screenshot = false;
 % Generative properties
 how_many_frames     = 150;
 frames_per_second   = 24;
-type_of_game        = 'random'; % 'picture'
+type_of_game        = 'picture'; % 'picture'
 cell_size           = 10;
+border_size         = 0;
+blank_frames        = 10;
+direction           = 'reverse'; % 'forward'
 
 switch type_of_game
     case 'random'
@@ -38,12 +41,47 @@ switch type_of_game
         
     case 'picture'
         % Load the picture
+        the_foundation  = imread('../images/palindrone_youtube.png');
         
         % Determine the dimensions
+        dimensions      = [size(the_foundation,1),size(the_foundation,2)];
         
         % Determine the colours
+        colour(1,:) = [0,0,0]; % Black
+        colour(2,:) = [255, 247, 210]; % Cream
+        colour(3,:) = [254, 104, 51]; % Ochre
+              
+        % Define the block size
+        block_size  = [cell_size, cell_size];
         
-        % Populate board
+        % Function to calculate the mean of each block
+        mean_filter = @(block_struct) mean2(block_struct.data);
+        
+        % Apply the block processing function to downsize the matrix
+        image_board = blockproc(the_foundation(:,:,3), block_size, mean_filter);
+        
+        image_board(image_board == 0)   = 1;
+        image_board(image_board == 210) = 2;
+        image_board(image_board == 51)  = 3;
+        
+        % Populate noise board
+        noise_board     = randi(...
+            size(colour,1),...
+            dimensions(1)/cell_size, dimensions(2)/cell_size);
+        
+        % Add X% random noise 
+        noise_percent   = .01;
+        
+        small_dims = dimensions ./ cell_size;
+        
+        % Select these pixels
+        these_pixels    = randperm(...
+            small_dims(1) * small_dims(2), ...
+            round( (small_dims(1)*small_dims(2)) * noise_percent));
+        
+        % Blend image & noise
+        the_board               = image_board;
+        the_board(these_pixels) = noise_board(these_pixels);
 end
 
 %% GENERATE GAME OF LIFE
@@ -57,16 +95,23 @@ the_array = cell(1,how_many_frames);
 for the_frame = 1:how_many_frames
    
     % Evolve the board
-    the_board   = evolve_life(the_board);
+    if the_frame > blank_frames
+        the_board   = evolve_life(the_board);
+    end
     
     % Resize
-    the_vision  = generate_board(the_board,cell_size);
+    the_vision  = generate_board(the_board,cell_size,border_size);
     
     % Recolour
     the_image   = colourise(the_vision, colour);
     
     % Save into array
-    the_array{the_frame} = the_image;
+    switch direction
+        case 'forward'
+            the_array{the_frame} = the_image;
+        case 'reverse'
+            the_array{how_many_frames - the_frame + 1} = the_image;
+    end
     
     fprintf('%3.0d / %3.0d\n',the_frame, how_many_frames);
 end
@@ -105,6 +150,8 @@ for the_frame = 1:how_many_frames
     % Wait
     WaitSecs(1/frames_per_second);
 end
+
+WaitSecs(2);
 
 %% CLOSE WINDOW
 
